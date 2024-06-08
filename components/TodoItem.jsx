ca0f56ba2bdfa5a2551e810e72ad06e5dca68a08
Kanikayudha Tantyo N <kanikayudha.17@students.amikom.ac.id>
2024-06-08 09:55:08 +0700

    Develop toggle todo feature

--- components/TodoItem.jsx
+++ components/TodoItem.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, toggleCompleted }) => {
+  const getTodoTitleStyle = () => {
+    if (todo.completed === true) {
+      return { textDecoration: 'line-through' }
+    } else {
+      return { textDecoration: 'none' }
+    }
+  }
+
   return (
-    <div style={style.todoItem}>
-      <p>{todo.title}</p>
+    <div style={styles.todoItem}>
+      <input 
+      type="checkbox" 
+      style={styles.checkbox}
+      onChange={() => toggleCompleted(todo.id)} 
+      />
+      <p style={getTodoTitleStyle()}>{todo.title}</p>
     </div>
   )
 }
 
-const style = {
+const styles = {
   todoItem: {
     border: '2px solid #f4f4f4',
     fontSize: '24px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  checkbox: {
+    marginRight: '16px',
+    height: '18px',
+    width: '18px',
   },
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,22 @@ function App(){
       completed: false,
     },
   ]);
-  
   console.log(todos)
+
+  const toggleCompleted = (todoid) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todoid === todo.id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
 
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-      <Todos todos={todos}/>
+      <Todos todos={todos} toggleCompleted={toggleCompleted}/>
     </div>
   )
 }  
